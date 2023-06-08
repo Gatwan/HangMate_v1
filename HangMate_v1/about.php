640e091240e6f1994f008cc34fe497b147a1ba70
Gatwan <111293749+Gatwan@users.noreply.github.com>
2023-06-08 13:53:28 +0300

    Add hangmate from local machine

---
+++ HangMate_v1/about.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+
+    <!--swiper css link-->
+    <!--<link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">-->
+
+    <!--font awesome cdn link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <!--js breakpoint swiper link-->
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/swiper/swiper-bundle.min.css"
+    />
+
+    <!--custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+     
+<!--header section starts-->
+
+<section class="header">
+
+    <a href="home.php" class="logo">HangMate</a>
+    
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="listing.php">listing</a>
+        <a href="book.php">book</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars">
+
+    </div>
+</section>
+<!--header section ends-->
+
+<div class="heading" style="background:url(images/aboutcover.jpg) no-repeat">
+    <h1>About us</h1>
+</div>
+
+<!--about section starts-->
+
+<section class="about">
+
+    <div class="image">
+        <img src="images/about-img.jpg" alt="">
+    </div>
+
+    <div class="content">
+        <h3>why choose us?</h3>
+        <p>Discover, plan, and create unforgettable moments with friends! We connect you with local activities, plan unforgettable hangouts and capture worthwhile memories.</p>
+        <p>Join us now to explore, connect and make every moment count</p>
+        <div class="icons-container">
+            <div class="icons">
+                <i class="fas fa-map"></i>
+                <span>Top destinations</span>
+            </div>
+            <div class="icons">
+                <i class="fas fa-hand-holding-usd"></i>
+                <span>affordable price</span>
+            </div>
+            <div class="icons">
+                <i class="fas fa-headset"></i>
+                <span>24/7 bookings</span>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<!--about section ends-->
+
+<!--reviews section starts-->
+
+<section class="reviews">
+
+    <div class="swiper reviews-slider">
+
+        <div class="swiper-wrapper">
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>This page is a game changer! It effortlessly plans exciting activities. I love how easy it is to discover local events and create memorable moments. Highly recommended!</p>
+                <h3>Sarah</h3>
+                <img src="images/r-sarah.jpg" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>I can't imagine my social life without this hangout app. It has become my go-to platform for discovering new places. A must-have!</p>
+                <h3>Alex</h3>
+                <img src="images/r-2.jpg" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Finally, an app that simplifies hangout planning. It's user friendly and has a wide range of activities to choose from.</p>
+                <h3>Chloe</h3>
+                <img src="images/r-chloe.jpg" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>I'm amazed by the seamless experience this hangout app provides. From planning hangouts with friends to exploring unique activities, it has everything covered.</p>
+                <h3>Happy explorer</h3>
+                <img src="images/r-1.jpg" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>This hangout app has revolutionized how I spend my free time. It's engaging, and offers a wide variety of events and hangout options.</p>
+                <h3>Adventure Seeker</h3>
+                <img src="images/r-51.jpeg" alt="">
+            </div>
+
+        </div>
+    </div>
+
+</section>
+<!--reviews section ends-->
+
+
+
+
+
+
+
+
+
+
+
+
+<!--footer section starts-->
+
+<section class="footer">
+
+    <div class="box-container">
+
+    <div class="box">
+        <h3>quick links</h3>
+        <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+        <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+        <a href="listing.php"> <i class="fas fa-angle-right"></i> listing</a>
+        <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+    </div>
+
+    <div class="box">
+        <h3>extra links</h3>
+        <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+    </div>
+
+    <div class="box">
+        <h3>contact info</h3>
+        <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+        <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+        <a href="#"> <i class="fas fa-envelope"></i> info@hangmate.com </a>
+        <a href="#"> <i class="fas fa-map"></i> nairobi, kenya - 00200 </a>
+     </div>
+
+     <div class="box">
+        <h3>Follow us</h3>
+        <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+        <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+        <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+        <a href="#"> <i class="fab fa-linkedin"></i> LinkedIn </a>
+    </div>
+
+    </div>
+
+<div class="credit"> Created by <span> HangMate</span> | all rights reserved! </div>
+
+</section>
+
+<!--footer section ends-->
+
+
+
+
+
+
+
+
+
+
+
+<!--swiper js link-->
+<script src="https://codesandbox.io/s/3blv5"></script>
+
+<!--custom js file link-->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ HangMate_v1/listing.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+
+    <!--swiper css link-->
+    <!--<link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">-->
+
+    <!--font awesome cdn link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <!--custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+     
+<!--header section starts-->
+
+<section class="header">
+
+    <a href="home.php" class="logo">HangMate</a>
+    
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="listing.php">listing</a>
+        <a href="book.php">book</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars">
+
+    </div>
+</section>
+<!--header section ends-->
+
+<div class="heading" style="background:url(images/aboutcover.jpg) no-repeat">
+    <h1>Packages</h1>
+</div>
+
+<!--listing section starts-->
+
+<section class="listings">
+
+    <h1 class="heading-title">Top destinations</h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="images/bowl.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Strike Frenzy</h3>
+                <p>Discover lanes, roll balls, knock pins and score big in the world of bowling. Let's chase those perfect shots and create unforgettable memories together! #BowlingAdventure</p>
+                <a href="book.php" class="btn">Book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/fgiraffe.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Tall Munchies</h3>
+                <p>I heard the giraffe was feeling a bit 'neck'-y, so I offered it a delectable treat. Experience the thrill of feeding a giraffe, an encounter like no other!</p>
+                <a href="book.php" class="btn">Book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/happyhour.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Cheerful Mix</h3>
+                <p>Unwind, sip, and immerse yourself in a vibrant ambience. Cheers to good times, great company, and unforgettable moments.</p>
+                <a href="book.php" class="btn">Book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/mtkenya.png" alt="">
+            </div>
+            <div class="content">
+                <h3>Summit Stride</h3>
+                <p>Lace up those boots, embrace the uphill adventure, and conquer mountains with a smile. Leave no path untrodden, as you hike with joy and relish in the euphoria of reaching the summit.</p>
+                <a href="book.php" class="btn">Book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/pottery.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Clay Play</h3>
+                <p>Get your hands dirty, unleash your inner artist, and experience the sheer joy of crafting unique pottery. Let the wheel spin and let your creativity soar!</p>
+                <a href="book.php" class="btn">Book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/rck_climb.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Grip Trip</h3>
+                <p>Embark on a gravity-defying adventure in indoor rock climbing. Feel the rush as you ascend to new heights, leaving no handhold explored. The thrill of the climb will elevate your spirits!</p>
+                <a href="book.php" class="btn">Book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/bowl.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Adventure & tour</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi aperiam maxime odit facilis repellendus magnam nemo impedit ut odio. Itaque, corrupti! Assumenda consequatur dignissimos eaque quas minima repellendus sint itaque.</p>
+                <a href="book.php" class="btn">Book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/bowl.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Adventure & tour</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi aperiam maxime odit facilis repellendus magnam nemo impedit ut odio. Itaque, corrupti! Assumenda consequatur dignissimos eaque quas minima repellendus sint itaque.</p>
+                <a href="book.php" class="btn">Book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/bowl.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Brush Bliss</h3>
+                <p>Immerse yourself in a world of colors, where brushes dance and creativity blooms. Dip your brush into a palette of joy, create strokes of brilliance, and let the canvas be your playground.</p>
+                <a href="book.php" class="btn">Book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/bowl.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Adventure & tour</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi aperiam maxime odit facilis repellendus magnam nemo impedit ut odio. Itaque, corrupti! Assumenda consequatur dignissimos eaque quas minima repellendus sint itaque.</p>
+                <a href="book.php" class="btn">Book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/bowl.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Adventure & tour</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi aperiam maxime odit facilis repellendus magnam nemo impedit ut odio. Itaque, corrupti! Assumenda consequatur dignissimos eaque quas minima repellendus sint itaque.</p>
+                <a href="book.php" class="btn">Book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/bowl.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Adventure & tour</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi aperiam maxime odit facilis repellendus magnam nemo impedit ut odio. Itaque, corrupti! Assumenda consequatur dignissimos eaque quas minima repellendus sint itaque.</p>
+                <a href="book.php" class="btn">Book now</a>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="load-more"><span class="btn">load more</span></div>
+    
+</section>
+<!--Listing section ends-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--footer section starts-->
+
+<section class="footer">
+
+    <div class="box-container">
+
+    <div class="box">
+        <h3>quick links</h3>
+        <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+        <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+        <a href="listing.php"> <i class="fas fa-angle-right"></i> listing</a>
+        <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+    </div>
+
+    <div class="box">
+        <h3>extra links</h3>
+        <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+    </div>
+
+    <div class="box">
+        <h3>contact info</h3>
+        <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+        <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+        <a href="#"> <i class="fas fa-envelope"></i> info@hangmate.com </a>
+        <a href="#"> <i class="fas fa-map"></i> nairobi, kenya - 00200 </a>
+     </div>
+
+     <div class="box">
+        <h3>Follow us</h3>
+        <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+        <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+        <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+        <a href="#"> <i class="fab fa-linkedin"></i> LinkedIn </a>
+    </div>
+
+    </div>
+
+<div class="credit"> Created by <span> HangMate</span> | all rights reserved! </div>
+
+</section>
+
+<!--footer section ends-->
+
+
+
+
+
+
+
+
+
+
+
+<!--swiper js link-->
+<!--<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>-->
+
+<!--custom js file link-->
+<script src="js/script.js"></script>
+</body>
+</html>
